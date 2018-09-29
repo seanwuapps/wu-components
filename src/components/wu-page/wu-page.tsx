3d3e1d8525e0b5f0ab6c90ba5c8513ea847409fa
Sean Wu <sean@seanwuapps.com>
2018-09-30 09:44:24 +1000

    reflect fixed header class

--- src/components/wu-page/wu-page.tsx
+++ src/components/wu-page/wu-page.tsx
@@ -1,10 +1,19 @@
-import { Component } from '@stencil/core'
+import { Component, Prop, Element } from '@stencil/core'
 
 @Component({
   tag: 'wu-page',
   styleUrl: 'wu-page.scss'
   })
 export class WuPage {
+  @Element()
+  el: HTMLElement
+  @Prop()
+  reflectFixedHeader?: boolean
+  componentDidLoad () {
+    if (this.reflectFixedHeader) {
+      this.el.classList.add('.reflect-fixed-header')
+    }
+  }
   render () {
     return <slot />
   }
