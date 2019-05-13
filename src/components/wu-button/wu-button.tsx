2fb7e43aefa7751a6b74453ccd7520e9d99b9e6c
Sean Wu <sean.wu@cordelta.com>
2019-05-13 10:21:15 +1000

    move attribute style to classes

--- src/components/wu-button/wu-button.tsx
+++ src/components/wu-button/wu-button.tsx
@@ -1,66 +1,83 @@
-import { Component, Element, Prop } from '@stencil/core'
+import { Component, Element, Prop } from "@stencil/core";
 
 @Component({
-  tag: 'wu-button',
-  styleUrls: ['vars.css', 'wu-button.scss']
-  })
+  tag: "wu-button",
+  styleUrls: ["vars.css", "wu-button.scss"]
+})
 export class WuButton {
-  @Element() button: HTMLElement
+  @Element() button: HTMLElement;
   /**
    * If `href` attribute is specified, the button will be output as an `a` tag
    */
-  @Prop() href?: string
+  @Prop() href?: string;
 
   /**
    * Html type of the button
    */
-  @Prop() type: 'submit' | 'reset' | 'button' = 'button'
+  @Prop() type: "submit" | "reset" | "button" = "button";
 
   /**
    * If this button is disabled
    */
-  @Prop({ reflectToAttr: true }) disabled: boolean = false
+  @Prop({ reflectToAttr: true }) disabled: boolean = false;
 
   /**
    * Button theme
    */
-  @Prop({ reflectToAttr: true }) theme?: string
+  @Prop({ reflectToAttr: true }) theme?: string;
 
   /**
    * Shape of the button
    */
-  @Prop({ reflectToAttr: true }) shape?: 'circle' | 'round'
+  @Prop({ reflectToAttr: true }) shape?: "circle" | "round";
 
   /**
    * If use outline styles
    */
-  @Prop({ reflectToAttr: true }) outline?: boolean
+  @Prop({ reflectToAttr: true }) outline?: boolean;
 
   /**
    * If button will extend to full width
    */
-  @Prop({ reflectToAttr: true }) block?: boolean
+  @Prop({ reflectToAttr: true }) block?: boolean;
 
   /**
    * If the button will use flat style
    */
-  @Prop({ reflectToAttr: true }) flat?: boolean
+  @Prop({ reflectToAttr: true }) flat?: boolean;
 
-  componentDidLoad () {
+  componentDidLoad() {
     if (this.theme) {
-      this.button.setAttribute('theme', this.theme)
+      this.button.setAttribute("theme", this.theme);
+    }
+
+    if (this.shape) {
+      this.button.classList.add(`shape-${this.shape}`);
+    }
+    if (this.outline) {
+      this.button.classList.add(`outline`);
+    }
+    if (this.block) {
+      this.button.classList.add(`block`);
+    }
+    if (this.flat) {
+      this.button.classList.add(`flat`);
     }
   }
 
-  onClick () {}
-  render () {
-    const TagType = this.href ? 'a' : 'button'
-    const attrs = TagType === 'button' ? { type: this.type } : { href: this.href }
+  onClick() {}
+  render() {
+    const TagType = this.href ? "a" : "button";
+    const attrs =
+      TagType === "button" ? { type: this.type } : { href: this.href };
 
     return (
-      <TagType {...attrs} disabled={this.disabled} onClick={this.onClick.bind(this)}>
+      <TagType
+        {...attrs}
+        disabled={this.disabled}
+        onClick={this.onClick.bind(this)}>
         <slot />
       </TagType>
-    )
+    );
   }
 }
--- src/components/wu-card/wu-card.tsx
+++ src/components/wu-card/wu-card.tsx
@@ -1,69 +1,89 @@
-import { Component, Prop, Element, State } from '@stencil/core'
-import { isSlotEmpty } from '../../utils'
+import { Component, Prop, Element, State } from "@stencil/core";
+import { isSlotEmpty } from "../../utils";
 @Component({
-  tag: 'wu-card',
-  styleUrls: ['vars.css', 'wu-card.scss']
-  })
+  tag: "wu-card",
+  styleUrls: ["vars.css", "wu-card.scss"]
+})
 export class WuCard {
   @Element()
-  el: HTMLElement
+  el: HTMLElement;
 
   /**
    * Title of the card
    */
   @Prop()
-  cardTitle?: string = ''
+  cardTitle?: string = "";
 
   /**
    * Card image source url, if specified, image will display on top of the card.
    */
   @Prop()
-  src?: string = ''
+  src?: string = "";
 
   /**
    * Thumbnail version of the card image, image will be loaded progressively if `thumb` is specified
    */
   @Prop()
-  thumb?: string = ''
+  thumb?: string = "";
 
   /**
    * Card image alt text
    */
   @Prop()
-  alt?: string = ''
+  alt?: string = "";
 
   /**
    * If this card is hoverable
    */
-  @Prop({ reflectToAttr: true }) hover?: boolean
+  @Prop({ reflectToAttr: true }) hover?: boolean;
 
   /**
    * If this card has flat (border) style
    */
-  @Prop({ reflectToAttr: true }) flat?: boolean
+  @Prop({ reflectToAttr: true }) flat?: boolean;
+
+  /**
+   * Make card themable
+   */
+  @Prop({ reflectToAttr: true }) theme?: boolean;
 
   @State()
-  hasFooter: boolean
+  hasFooter: boolean;
 
-  componentDidLoad () {
-    this.hasFooter = isSlotEmpty(this.el, 'footer')
+  componentDidLoad() {
+    this.hasFooter = isSlotEmpty(this.el, "footer");
+    if (this.hover) {
+      this.el.classList.add("hoverable");
+    }
+    if (this.flat) {
+      this.el.classList.add("flat");
+    }
   }
-  render () {
+  render() {
     return (
-      <div class="card">
-        {this.src && <wu-img class="card-media" src={this.src} thumb={this.thumb} alt={this.alt} />}
+      <div class='card'>
+        {this.src && (
+          <wu-img
+            class='card-media'
+            src={this.src}
+            thumb={this.thumb}
+            alt={this.alt}
+          />
+        )}
         <div>
-          {this.cardTitle && <div class="card-title" innerHTML={this.cardTitle} />}
-          <div class="card-body">
+          {this.cardTitle && (
+            <div class='card-title' innerHTML={this.cardTitle} />
+          )}
+          <div class='card-body'>
             <slot />
           </div>
         </div>
         {this.hasFooter && (
-          <div class="card-footer">
-            <slot name="footer" />
+          <div class='card-footer'>
+            <slot name='footer' />
           </div>
         )}
       </div>
-    )
+    );
   }
 }
